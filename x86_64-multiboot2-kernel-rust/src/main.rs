2eae019c0b4ea86f28e972217f81b06356e6bdcc
Philipp Schuster <phip1611@gmail.com>
2023-06-29 16:17:44 +0200

    wip i only read zeroes

--- x86_64-multiboot2-kernel-rust/src/main.rs
+++ x86_64-multiboot2-kernel-rust/src/main.rs
@@ -3,15 +3,15 @@
 //   2) we write Kernel code, but standard lib makes syscalls and is meant for userland programs
 #![no_std]
 #![no_main]
-
 #![feature(const_mut_refs)]
-
+#![feature(panic_info_message)]
 #![deny(missing_debug_implementations)]
 
 use crate::debugcon::Printer;
 use core::fmt::Write;
+use core::mem::size_of;
 use core::panic::PanicInfo;
-use multiboot2::BootInformation;
+use multiboot2::{BootInformation, BootInformationHeader};
 
 mod asm;
 mod global_alloc;
@@ -37,23 +37,47 @@ extern "C" fn kernel_entry(load_addr: u64, multiboot2_info: u64) -> ! {
     )
     .unwrap();
 
-    let mbi = multiboot2_info as *const u64 as *const multiboot2::BootInformationHeader;
-    let mbi = unsafe { BootInformation::load(mbi) }.unwrap();
+    #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    #[repr(C)]
+    pub struct MbiHeader {
+        // size is multiple of 8
+        total_size: u32,
+        _reserved: u32,
+        // Followed by the boot information tags.
+    }
+    // Multiboot info within first 2 MiB (identity mapped as huge page)
+    // I assume that GRUB is friendly here. This is no production-ready solution.
+    assert!(multiboot2_info <= 0x1ff000);
+    let mbi_ptr = multiboot2_info as *const u64 as *const MbiHeader;
+    let bytes = unsafe {core::slice::from_raw_parts(mbi_ptr.cast::<u8>(), 32)};
+    panic!("{bytes:#x?}"); /*
+                        //let mbi = unsafe { BootInformation::load(mbi) }.unwrap();
+
+                        let mbi_raw = unsafe { mbi_ptr.cast::<u8>().add(size_of::<BootInformationHeader>()) };
+                        // let mbi_raw_slice = unsafe { core::slice::from_raw_parts(mbi_raw, (mbi.total_size - 4) as usize) };
+
+                        writeln!(Printer, "{:#?}", mbi_raw_slice).unwrap();
 
 
-    writeln!(
-        Printer,
-        "Multiboot2 boot info: {:?}",
-        mbi
-    )
-        .unwrap();
+                        writeln!(
+                            Printer,
+                            "Multiboot2 boot info: {:?}",
+                            mbi
+                        )
+                            .unwrap();*/
 
     loop {}
 }
 
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
-    write!(Printer, "PANIC: {:#?}", info).unwrap();
+    let _ = write!(
+        Printer,
+        "PANIC: [{}:{}]: {:?}",
+        info.location().map(|l| l.file()).unwrap_or("<unknown>"),
+        info.location().map(|l| l.line()).unwrap_or(0),
+        info.message().unwrap(),
+    );
     loop {}
 }
 
